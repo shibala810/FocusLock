e197fa8cdcaf670b0487284f99c81d264a22089b
timwu <shibala810@gmail.com>
2026-05-30 12:12:27 +0800

    Initial Commit

---
+++ FocusLockTests/FocusLockTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusLockTests.swift
+//  FocusLockTests
+//
+//  Created by timwu on 2026/5/30.
+//
+
+import Testing
+@testable import FocusLock
+
+struct FocusLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
